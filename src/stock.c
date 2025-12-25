e39656ae05f95af476f352df40b31ec6fba2a26c
Raj Patel <raj202patel@gmail.com>
2025-12-25 13:38:59 -0700

    update: add fetch method on monitor

--- src/stock.c
+++ src/stock.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "stock.h"
 #include <string.h>
+#include <curl/curl.h>
+#include <stdlib.h>
 
 Stock create_stock(const char *ticker, double price, double change)
 {
     Stock s;
-    strncpy(s.ticker, ticker, sizeof(s.ticker));
+    strncpy(s.ticker, ticker, sizeof(s.ticker) - 1);
+    s.ticker[sizeof(s.ticker) - 1] = '\0';
     s.price = price;
     s.change = change;
     return s;
@@ -16,3 +19,10 @@ void print_stock(const Stock *s)
     // printf("%s: $%.2f (%.2f%%)\n", s->ticker, s->price, s->change);
     printf("%s: $%.2f (%.2f%%)\n", s->ticker, s->price, s->change);
 }
+
+// CURL write callback
+struct Memory
+{
+    char *data;
+    size_t size;
+};
--- src/stock.h
+++ src/stock.h
@@ -16,4 +16,7 @@ Stock create_stock(const char *ticker, double price, double change);
 // Print Statement
 void print_stock(const Stock *s);
 
+// Fetch stock price from Yahoo Finance
+int fetch_stock_price(Stock *s);
+
 #endif
